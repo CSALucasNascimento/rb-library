4752b4450d12688f6dabac8f63b76e14c61a0171
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-11-14 18:22:48 +1100

    First Commit raw component

---
+++ src/Component/Component.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Component.scss'
+
+class Component extends React.Component {
+
+    render() {
+        return (
+            <div className={`component`}>Component</div>
+        )
+    }
+
+}
+
+export default Component
